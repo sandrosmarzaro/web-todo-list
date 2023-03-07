1732bae5aca2d0aa2dc57017f3c1262ba66c587a
Sandro Smarzaro <sansmarzaro@gmail.com>
2023-03-07 16:48:47 -0300

    fix: wrong primary key in repositories

--- src/main/java/io/virtual360/todolist/repositories/TodoListRepository.java
+++ src/main/java/io/virtual360/todolist/repositories/TodoListRepository.java
@@ -4,6 +4,6 @@ package io.virtual360.todolist.repositories;
 import io.virtual360.todolist.models.TodoList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoListRepository extends JpaRepository<TodoList, String> {
+public interface TodoListRepository extends JpaRepository<TodoList, Integer> {
 
 }
--- src/main/java/io/virtual360/todolist/repositories/TodoRepository.java
+++ src/main/java/io/virtual360/todolist/repositories/TodoRepository.java
@@ -3,5 +3,5 @@ package io.virtual360.todolist.repositories;
 import io.virtual360.todolist.models.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepository extends JpaRepository<Todo, String> {
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
 }
